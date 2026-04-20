1febb31745e4ac18211f24476b5aac831047e95d
Arman Dezfuli <armandezfuli@proton.me>
2026-04-20 11:59:34 +0330

    chore: cleanup comments

--- src/components/Kanban/Kanban.tsx
+++ src/components/Kanban/Kanban.tsx
@@ -1,82 +1,13 @@
 import styles from "./Kanban.module.css"
 import { KanbanHeader } from "../KanbanHeader/KanbanHeader"
 import { KanbanBoards } from "../KanbanBoards/KanbanBoards"
-// import type { KanbanStatus } from "../../types/kanban"
-import {type ReactNode } from "react"
-// import useKanban from "../../context/KanbanContext/useKanban"
+import { type ReactNode } from "react"
 
 export function Kanban(): ReactNode {
-    // const { kanbanData, dispatch } = useKanban()
-
-    // const [selectedCard, setSelectedCard] = useState<null | string>(null)
-    // const kanbanRef = useRef<HTMLElement | null>(null)
-
-    // useEffect(() => {
-    //     const handleClickOutside = (event: MouseEvent) => {
-    //         if (!kanbanRef.current) return
-
-    //         if (!kanbanRef.current.contains(event.target as Node)) {
-    //             setSelectedCard(null)
-    //         }
-    //     }
-
-    //     document.addEventListener("pointerdown", handleClickOutside)
-
-    //     return () => {
-    //         document.removeEventListener("pointerdown", handleClickOutside)
-    //     }
-    // }, [])
-
-    // const handleSelectCard = useCallback((id: string) => {
-    //     setSelectedCard((prev) => (prev === id ? null : id))
-    // }, [])
-
-    // const selectedCardData = useMemo(
-    //     () => kanbanData.cards.find((card) => card.id === selectedCard),
-    //     [kanbanData]
-    // )
-
-    // const handleDeleteCard = useCallback(() => {
-    //     if (!selectedCard) return
-    //     setKanbanData((prev) => ({
-    //         ...prev,
-    //         cards: prev.cards.filter((c) => c.id !== selectedCard),
-    //     }))
-    //     setSelectedCard(null)
-    // }, [selectedCard])
-
-    // const handleMoveCard = useCallback(
-    //     (status: KanbanStatus) => {
-    //         if (!selectedCard) return
-
-    //         setKanbanData((prev) => ({
-    //             ...prev,
-    //             cards: prev.cards.map((card) =>
-    //                 card.id === selectedCard ? { ...card, status } : card
-    //             ),
-    //         }))
-    //     },
-    //     [selectedCard]
-    // )
-
     return (
-        <section
-            className={styles.kanban}
-            // ref={kanbanRef}
-            // onClick={() => setSelectedCard(null)}
-        >
-            <KanbanHeader
-                // title={kanbanData.title}
-                // selectedCardId={selectedCard}
-                // onDeleteCard={handleDeleteCard}
-                // onMoveCard={handleMoveCard}
-                // selectedCardStatus={selectedCardData?.status ?? null}
-            />
-            <KanbanBoards
-                // kanbanData={kanbanData}
-                // onSelectCard={handleSelectCard}
-                // selectedCard={selectedCard}
-            />
+        <section className={styles.kanban}>
+            <KanbanHeader />
+            <KanbanBoards />
         </section>
     )
 }
